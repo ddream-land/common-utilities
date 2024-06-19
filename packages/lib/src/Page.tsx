24a4548f833a3654721d8176d8c1158ce19dafe5
jackietang <tanghj@cdszcs.com>
2024-06-19 16:38:10 +0800

    fix: Common lib tailwind. feat: Payment confirm.

--- packages/lib/src/Page.tsx
+++ packages/lib/src/Page.tsx
@@ -1,22 +1,23 @@
-import { useEffect, useState } from 'react'
 import { DDLPay } from './components/pay/DDLPay'
-import { NextUIProvider } from '@nextui-org/react'
 import { useDisclosure } from '@nextui-org/react'
-import { http } from './utils/http'
 
 function Page() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure({ defaultOpen: false })
 
   return (
-    <NextUIProvider className="w-full h-full">
-      <div className="w-full h-full bg-white">
-        <div className="bg-white text-black" onClick={() => onOpen()}>
-          Pay
-        </div>
-        {/* <DDLPay></DDLPay> */}
-        <DDLPay isOpen={isOpen} onOpenChange={onOpenChange}></DDLPay>
+    <div className="w-full h-full bg-white">
+      <div className="bg-white text-black" onClick={() => onOpen()}>
+        Pay
       </div>
-    </NextUIProvider>
+      <DDLPay
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        onPaymentComplete={() => {
+          console.log('payment complete')
+        }}
+        lang="zh-CN"
+      ></DDLPay>
+    </div>
   )
 }
 
--- packages/lib/src/components/pay/DDLPay.tsx
+++ packages/lib/src/components/pay/DDLPay.tsx
@@ -1,4 +1,4 @@
-import { Button, Image } from '@nextui-org/react'
+import { Button, Image, NextUIProvider } from '@nextui-org/react'
 import classes from './DDLPay.module.scss'
 import { useLanguageT } from '../../useLanguageT'
 import { SupportLangs } from '../../types/SupportLangs'
@@ -8,9 +8,9 @@ import { http } from '../../utils/http'
 import { useLanguageManager } from '../../useLanguageManager'
 
 export type DDLPayProps = Readonly<{
-  className?: string
   isOpen: boolean
   onOpenChange: (isOpen: boolean) => void
+  onPaymentComplete?: () => void
   lang?: SupportLangs
 }>
 
@@ -21,7 +21,7 @@ type Price = {
   product_num: number
 }
 
-function DDLPay({ isOpen, onOpenChange, className, lang }: DDLPayProps) {
+function DDLPay({ isOpen, onOpenChange, onPaymentComplete, lang }: DDLPayProps) {
   const {} = useLanguageManager(lang)
 
   const { translation: tPurchase } = useLanguageT('purchase')
@@ -33,9 +33,13 @@ function DDLPay({ isOpen, onOpenChange, className, lang }: DDLPayProps) {
   const { translation: tHours } = useLanguageT('hours')
   const { translation: tMins } = useLanguageT('mins')
 
-  const [currentSelectPkgId, setCurrentSelectPkgId] = useState('')
+  const { translation: tPayment } = useLanguageT('payment')
+  const { translation: tPaymentDesc } = useLanguageT('paymentDesc')
+  const { translation: tPaymentCompleted } = useLanguageT('paymentCompleted')
 
+  const [currentSelectPkgId, setCurrentSelectPkgId] = useState('')
   const [payBtnLoading, setPayBtnLoading] = useState(false)
+  const [waitingCallback, setWaitingCallback] = useState(false)
 
   const [prices, setPrices] = useState<Price[]>([])
   const [config, setConfig] = useState({
@@ -83,6 +87,8 @@ function DDLPay({ isOpen, onOpenChange, className, lang }: DDLPayProps) {
   }, 60 * 1000)
 
   async function refreshPrices() {
+    setPrices([])
+
     const res = await http({
       url: `/api/v1/finance/sales`,
       init: {
@@ -154,8 +160,15 @@ function DDLPay({ isOpen, onOpenChange, className, lang }: DDLPayProps) {
       return
     }
 
+    setWaitingCallback(true)
+
     const url = data.url
     window.open(url, 'blank')
+  }
+
+  async function onPayCompleteClicked() {
+    onPaymentComplete && onPaymentComplete()
+    onOpenChange(!isOpen)
   }
 
   useEffect(function () {
@@ -165,8 +178,21 @@ function DDLPay({ isOpen, onOpenChange, className, lang }: DDLPayProps) {
     })()
   }, [])
 
+  useEffect(
+    function () {
+      if (!isOpen) {
+        setCurrentSelectPkgId('')
+        setPayBtnLoading(false)
+        setWaitingCallback(false)
+      } else {
+        refreshPrices()
+      }
+    },
+    [isOpen]
+  )
+
   return (
-    <>
+    <NextUIProvider>
       {isOpen && (
         <div
           onClick={() => {
@@ -174,99 +200,119 @@ function DDLPay({ isOpen, onOpenChange, className, lang }: DDLPayProps) {
           }}
           className="fixed z-50 inset-0 flex justify-center items-center bg-slate-500/60"
         >
-          <div
-            onClick={(e) => {
-              e.stopPropagation()
-            }}
-            className={`${classes.pay} ${
-              className ?? ''
-            } w-[1180px] h-[613px] bg-[#000000] rounded-[24px] border-[#333333] border-1 flex flex-col`}
-          >
+          {!waitingCallback && (
             <div
-              className={`${classes.title} h-[30px] mt-[42px] flex flex-row justify-center items-center`}
+              onClick={(e) => {
+                e.stopPropagation()
+              }}
+              className={`${classes.pay} w-[1180px] h-[613px] bg-[#000000] rounded-[24px] border-[#333333] border-1 flex flex-col`}
             >
-              <span>{tPurchase}</span>
-              <Image
-                className="mx-[12px]"
-                disableSkeleton={true}
-                width={90}
-                height={30}
-                src={dreamlandLogo}
-                alt="logo"
-              />
-              <span>{tDreamTokens}</span>
-            </div>
-
-            {onSaleState.show && (
-              <div className="mt-[15px] flex flex-row justify-center items-center">
-                <span
-                  className={`${classes.onSale} bg-[#111111] border-1 border-[#B9FF4C] text-[#B9FF4C] rounded-[30px] px-[22px] py-[10px] flex flex-row items-center`}
-                >
-                  <span>{tOnSaleEndDesc}</span>
-                  &nbsp;
-                  <span className=" font-bold text-[14px]">{onSaleState.days}</span>
-                  &nbsp;
-                  <span>{tDays}</span>
-                  &nbsp;
-                  <span className=" font-bold text-[14px]">{onSaleState.hours}</span>
-                  &nbsp;
-                  <span>{tHours}</span>
-                  &nbsp;
-                  <span className=" font-bold text-[14px]">{onSaleState.mins}</span>
-                  &nbsp;
-                  <span>{tMins}</span>
-                </span>
+              <div
+                className={`${classes.title} h-[30px] mt-[42px] flex flex-row justify-center items-center`}
+              >
+                <span>{tPurchase}</span>
+                <Image
+                  className="mx-[12px]"
+                  disableSkeleton={true}
+                  width={90}
+                  height={30}
+                  src={dreamlandLogo}
+                  alt="logo"
+                />
+                <span>{tDreamTokens}</span>
               </div>
-            )}
 
-            <div
-              className={`${
-                onSaleState.show ? 'mt-[52px]' : 'mt-[104px]'
-              } flex flex-row justify-center items-center gap-[20px]`}
-            >
-              {prices.map(function (price, index) {
-                return (
-                  <div
-                    key={index}
-                    onClick={(e) => {
-                      setCurrentSelectPkgId(price.package_id)
-                    }}
-                    className={`${classes.pricePanel} ${
-                      currentSelectPkgId === price.package_id ? classes.selected : ''
-                    } cursor-pointer w-[180px] h-[240px] border-1 border-[#353535] rounded-[14px] flex flex-row justify-center items-center relative`}
-                    style={{
-                      backgroundImage: `url(${priceBg})`,
-                    }}
+              {onSaleState.show && (
+                <div className="mt-[15px] flex flex-row justify-center items-center">
+                  <span
+                    className={`${classes.onSale} bg-[#111111] border-1 border-[#B9FF4C] text-[#B9FF4C] rounded-[30px] px-[22px] py-[10px] flex flex-row items-center`}
                   >
-                    <div className="flex flex-row items-center justify-center">
-                      <div className={`${classes.dol}`}>$</div>
-                      <div className={`${classes.price} ml-1`}>{calcPrice(price.price)}</div>
-                    </div>
+                    <span>{tOnSaleEndDesc}</span>
+                    &nbsp;
+                    <span className=" font-bold text-[14px]">{onSaleState.days}</span>
+                    &nbsp;
+                    <span>{tDays}</span>
+                    &nbsp;
+                    <span className=" font-bold text-[14px]">{onSaleState.hours}</span>
+                    &nbsp;
+                    <span>{tHours}</span>
+                    &nbsp;
+                    <span className=" font-bold text-[14px]">{onSaleState.mins}</span>
+                    &nbsp;
+                    <span>{tMins}</span>
+                  </span>
+                </div>
+              )}
 
-                    <div className="absolute bottom-[20px] left-1/2 -translate-x-1/2 h-[24px] w-full flex flex-row justify-center items-baseline">
-                      <div className={`${classes.token}`}>{price.product_num}</div>
-                      <div className={`${classes.tokenUnit} ml-1`}>{tDreamToken}</div>
-                    </div>
-                  </div>
-                )
-              })}
-            </div>
+              <div
+                className={`${
+                  onSaleState.show ? 'mt-[52px]' : 'mt-[104px]'
+                } flex flex-row justify-center items-center gap-[20px]`}
+              >
+                {prices.map(function (price, index) {
+                  return (
+                    <div
+                      key={index}
+                      onClick={(e) => {
+                        setCurrentSelectPkgId(price.package_id)
+                      }}
+                      className={`${classes.pricePanel} ${
+                        currentSelectPkgId === price.package_id ? classes.selected : ''
+                      } cursor-pointer w-[180px] h-[240px] border-1 border-[#353535] rounded-[14px] flex flex-row justify-center items-center relative`}
+                      style={{
+                        backgroundImage: `url(${priceBg})`,
+                      }}
+                    >
+                      <div className="flex flex-row items-center justify-center">
+                        <div className={`${classes.dol}`}>$</div>
+                        <div className={`${classes.price} ml-1`}>{calcPrice(price.price)}</div>
+                      </div>
 
-            <div className="flex flex-row justify-center items-center mt-[90px]">
-              <Button
-                onClick={onPayClicked}
-                disabled={!currentSelectPkgId}
-                isLoading={payBtnLoading}
-                className={`w-[313px] h-[62px] rounded-[16px] cursor-pointer`}
-                style={{
-                  backgroundImage: `url(${payWithStripe})`,
-                }}
-              ></Button>
+                      <div className="absolute bottom-[20px] left-1/2 -translate-x-1/2 h-[24px] w-full flex flex-row justify-center items-baseline">
+                        <div className={`${classes.token}`}>{price.product_num}</div>
+                        <div className={`${classes.tokenUnit} ml-1`}>{tDreamToken}</div>
+                      </div>
+                    </div>
+                  )
+                })}
+              </div>
+
+              <div className="flex flex-row justify-center items-center mt-[90px]">
+                <Button
+                  onClick={onPayClicked}
+                  isDisabled={!currentSelectPkgId}
+                  isLoading={payBtnLoading}
+                  className={`w-[313px] h-[62px] rounded-[16px] cursor-pointer`}
+                  style={{
+                    backgroundImage: `url(${payWithStripe})`,
+                  }}
+                ></Button>
+              </div>
             </div>
-          </div>
+          )}
+
+          {waitingCallback && (
+            <div
+              onClick={(e) => {
+                e.stopPropagation()
+              }}
+              className={`${classes.payment} w-[558px] h-[300px] p-[34px] bg-[#121315] rounded-[12px] border-[#333333] border-1 flex flex-col`}
+            >
+              <div className={`${classes.title}`}>{tPayment}</div>
+              <div className={`${classes.desc} mt-[80px] text-[#A0A0AA]`}>{tPaymentDesc}</div>
+              <div className="mt-[50px] flex flex-row justify-end items-center">
+                <Button
+                  onClick={onPayCompleteClicked}
+                  className={`${classes.btn} w-[228px] rounded-[12px] bg-[#18A9CC]`}
+                >
+                  {tPaymentCompleted}
+                </Button>
+              </div>
+            </div>
+          )}
         </div>
       )}
-    </>
+    </NextUIProvider>
   )
 }
 
