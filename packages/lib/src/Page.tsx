5b8e9ac265435f8c07347dce53b919d28e177840
jackietang <tanghj@cdszcs.com>
2024-06-19 14:38:15 +0800

    feat: Update for demo.

--- packages/lib/src/Page.tsx
+++ packages/lib/src/Page.tsx
@@ -10,8 +10,8 @@ function Page() {
   return (
     <NextUIProvider className="w-full h-full">
       <div className="w-full h-full bg-white">
-        <div className="bg-white" onClick={() => onOpen()}>
-          Switch Open
+        <div className="bg-white text-black" onClick={() => onOpen()}>
+          Pay
         </div>
         {/* <DDLPay></DDLPay> */}
         <DDLPay isOpen={isOpen} onOpenChange={onOpenChange}></DDLPay>
