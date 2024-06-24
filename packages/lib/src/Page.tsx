dc1cff86352777d69b4d1617b47adaa340aaa808
Jack <thj3360016@gmail.com>
2024-06-24 22:56:14 +0800

    feat: Update DDLSidebar hover style.

--- packages/lib/src/Page.tsx
+++ packages/lib/src/Page.tsx
@@ -1,11 +1,13 @@
 import { DDLPay } from './components/pay/DDLPay'
 import { useDisclosure } from '@nextui-org/react'
+import { DDLSidebar } from './components/sidebar/DDLSidebar'
 
 function Page() {
   const { isOpen, onOpen, onOpenChange } = useDisclosure({ defaultOpen: false })
 
   return (
     <div className="w-full h-full bg-white">
+      <DDLSidebar title={{ name: '123' }}></DDLSidebar>
       <div className="bg-white text-black" onClick={() => onOpen()}>
         Pay
       </div>
